64ac9eace48195d185e61246f16050706d92f8de
Gergana Karabelyova <gergana.karabelyova@gmail.com>
2024-03-20 10:08:20 +0200

    Add files via upload

---
+++ C++/2235-add-two-integers.cpp
@@ -0,0 +1,25 @@
+/*  This code was written by Gergana Karabelyova as an exercise
+    on the C++ coding language. The text and conditions of the puzzle
+    are written below, after the code.  */
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
+
+
+
+/*
+
+Given two integers num1 and num2, return the sum of the two integers.
+ 
+
+Example 1:
+
+Input: num1 = 12, num2 = 5
+Output: 17
+
+
+*/
